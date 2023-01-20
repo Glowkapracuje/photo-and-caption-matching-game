828b8762c8d5d4fc9ed11e319ddd71d24f51d7ad
Glowkapracuje <piotr@glowkapracuje.pl>
2023-01-20 19:48:09 +0100

    Add a new Accordion widget type
    - Accordion widget allows to display title and expandable body text

---
+++ src/widgets/accordion-widget/accordion-widget.js
@@ -0,0 +1,58 @@
+const AccordionWidgetIIFE = (function () {
+    let defaultIconExpandedText = '🡱';
+    let defaultIconShortenedText = '🡳';
+
+    /**
+     * Generates an element that appears as an expandable accordion
+     * @param {{titleHtml: string, titleText: string, iconExpandedText: string, iconShortenedText: string, bodyText: string }} properties 
+     * @returns HTML element
+     */
+    function generateAccordionWidget(properties) {
+        let accordionWidgetContainer = document.createElement('div')
+        let headerWrapper = document.createElement('div');
+        let title = document.createElement('span')
+        let icon = document.createElement('span');
+        let bodyWrapper = document.createElement('div');
+
+        accordionWidgetContainer.setAttribute('class', 'accordion-widget-container');
+        headerWrapper.setAttribute('class', 'header-wrapper');
+        bodyWrapper.setAttribute('class', 'body-wrapper');
+        title.setAttribute('class', 'title');
+        icon.setAttribute('class', 'icon');
+
+        let titleHtml = properties.titleHtml || '';
+        let titleText = properties.titleText || '';
+        let iconExpandedText = properties.iconExpandedText || defaultIconExpandedText;
+        let iconShortenedText = properties.iconShortenedText || defaultIconShortenedText;
+        let bodyText = properties.bodyText;
+
+        headerWrapper.append(title, icon);
+        accordionWidgetContainer.append(headerWrapper, bodyWrapper);
+
+        if (titleHtml) {
+            title.innerHTML = titleHtml;
+        } else {
+            title.innerText = titleText;
+        }
+
+        icon.innerText = iconShortenedText;
+        bodyWrapper.innerText = bodyText;
+
+        headerWrapper.addEventListener('click', function () {
+            if (bodyWrapper.style.maxHeight) {
+                bodyWrapper.style.maxHeight = null;
+                icon.innerText = iconShortenedText;
+            } else {
+                bodyWrapper.style.maxHeight = bodyWrapper.scrollHeight + "px";
+                icon.innerText = iconExpandedText;
+            }
+        })
+
+        return accordionWidgetContainer;
+    }
+
+    return {
+        generateAccordionWidget: generateAccordionWidget,
+    }
+
+})();
